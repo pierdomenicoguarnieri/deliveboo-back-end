aacacf0ff8c2e633224b6fdac56b98310e4403de
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-07-12 18:19:21 +0200

    changed: register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,127 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container mt-4">
+  <div class="container mt-4">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Register</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+          <div class="card-body">
+            <form method="POST" action="{{ route('register') }}">
+              @csrf
 
-                        <div class="mb-4 row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+              <div class="mb-4 row">
+                <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                <div class="col-md-6">
+                  <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
+                    name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                                @error('name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="mb-4 row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="mb-4 row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="mb-4 row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="mb-4 row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                  @error('name')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror
                 </div>
-            </div>
+              </div>
+
+              <div class="mb-4 row">
+                <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+
+                <div class="col-md-6">
+                  <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                    name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                  @error('email')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror
+                </div>
+              </div>
+
+              <div class="mb-4 row">
+                <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+
+                <div class="col-md-6">
+                  <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                    name="password" required autocomplete="new-password">
+
+                  @error('password')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror
+                </div>
+              </div>
+
+              <div class="mb-4 row">
+                <label for="password-confirm"
+                  class="col-md-4 col-form-label text-md-right">Confirm Password</label>
+
+                <div class="col-md-6">
+                  <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required
+                    autocomplete="new-password">
+                </div>
+              </div>
+
+              <div class="mb-4 row">
+                <label for="resturant_name"
+                  class="col-md-4 col-form-label text-md-right">Restaurant Name</label>
+
+                <div class="col-md-6">
+                  <input id="resturant_name" type="text" class="form-control" name="resturant_name" required>
+                </div>
+              </div>
+
+              <div class="mb-4 row">
+                <label for="resturant_piva"
+                  class="col-md-4 col-form-label text-md-right">P. IVA</label>
+
+                <div class="col-md-6">
+                  <input id="resturant_piva" type="text" class="form-control" name="resturant_piva" required>
+                </div>
+              </div>
+
+              <div class="mb-4 row">
+                <label for="resturant_email"
+                  class="col-md-4 col-form-label text-md-right">Restaurant E-Mail Address</label>
+
+                <div class="col-md-6">
+                  <input id="resturant_email" type="email" class="form-control" name="resturant_email" required>
+                </div>
+              </div>
+
+              <div class="mb-4 row">
+                <label for="resturant_telephone_number"
+                  class="col-md-4 col-form-label text-md-right">Restaurant Phone Number</label>
+
+                <div class="col-md-6">
+                  <input id="resturant_telephone_number" type="text" class="form-control" name="resturant_telephone_number" required>
+                </div>
+              </div>
+
+              <div class="mb-4 row">
+                <label for="resturant_address"
+                  class="col-md-4 col-form-label text-md-right">Restaurant Address</label>
+
+                <div class="col-md-6">
+                  <input id="resturant_address" type="text" class="form-control" name="resturant_address" required>
+                </div>
+              </div>
+
+              <div class="mb-4 row mb-0">
+                <div class="col-md-6 offset-md-4">
+                  <button type="submit" class="btn btn-primary">
+                    Register
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
