68135fc5424654c37d6ecc83dbd72557c5dce1fa
emilioff8822 <emilioff@hotmail.it>
2023-07-12 16:49:59 +0200

    dishes type

---
+++ config/dishes.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'dishes' => [
+        'spaghetti_carbonara',
+        'penne_all_arrabbiata',
+        'lasagne',
+        'pizza_margherita',
+        'pizza_diavola',
+        'pollo_alla_cacciatora',
+        'melanzane_alla_parmigiana',
+        'fettuccine_alfredo',
+        'risotto_ai_funghi',
+        'linguine_alle_vongole',
+        'ravioli_di_spinaci_e_ricotta',
+        'osso_buco_alla_milanese',
+        'scaloppine_al_marsala',
+        'tagliatelle_alla_bolognese',
+        'pasta_primavera',
+        'scampi_fra_diavolo',
+        'pollo_al_marsala',
+        'bruschetta_al_pomodoro',
+        'tiramisu',
+        'cannoli_siciliani',
+    ]
+];
---
+++ config/types.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+return [
+    'types' => [
+        'italiano',
+        'sushi',
+        'pizza',
+        'pesce',
+        'cinese',
+        'indiano',
+        'messicano',
+        'vegetariano',
+        'vegano',
+        'fusion',
+        'mediterraneo',
+        'greco',
+        'francese',
+    ]
+];
