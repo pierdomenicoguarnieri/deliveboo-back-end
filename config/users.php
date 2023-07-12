2d4656731a19481540d3a9712b13a2933ccae13c
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-07-12 17:27:09 +0200

    add users, fix restaurants.php

---
+++ config/users.php
@@ -0,0 +1,70 @@
+<?php
+
+return [
+  [
+    "name"=> "Giovanni Filippo",
+    "email"=> "giovanni.filippo@gmail.com",
+    "email_verified_at"=> "",
+    "password"=> "12345678",
+  ],
+  [
+    "name"=> "Andrea Cavallo",
+    "email"=> "andrea.cavallo@gmail.com",
+    "email_verified_at"=> "",
+    "password"=> "12345678",
+  ],
+  [
+    "name"=> "Getrude Erberti",
+    "email"=> "gertrude.erberti@gmail.com",
+    "email_verified_at"=> "",
+    "password"=> "12345678",
+  ],
+  [
+    "name"=> "Isabella Bellaisa",
+    "email"=> "isabella.bellaisa@gmail.com",
+    "email_verified_at"=> "",
+    "password"=> "12345678",
+  ],
+  [
+    "name"=> "Anselmo Andante",
+    "email"=> "anselmo.andante@gmail.com",
+    "email_verified_at"=> "",
+    "password"=> "12345678",
+  ],
+  [
+    "name"=> "Belarda Conserva",
+    "email"=> "belarda.conserva@gmail.com",
+    "email_verified_at"=> "",
+    "password"=> "12345678",
+  ],
+  [
+    "name"=> "Antonio Tonelli",
+    "email"=> "antonio.tonelli@gmail.com",
+    "email_verified_at"=> "",
+    "password"=> "12345678",
+  ],
+  [
+    "name"=> "Bernarda Bernardi",
+    "email"=> "bernarda.bernardi@gmail.com",
+    "email_verified_at"=> "",
+    "password"=> "12345678",
+  ],
+  [
+    "name"=> "Giuseppe Infingardo",
+    "email"=> "giuseppe.infingardo@gmail.com",
+    "email_verified_at"=> "",
+    "password"=> "12345678",
+  ],
+  [
+    "name"=> "Balena Apnea",
+    "email"=> "balena.apnea@gmail.com",
+    "email_verified_at"=> "",
+    "password"=> "12345678",
+  ],
+  [
+    "name"=> "Carlo Magno",
+    "email"=> "carlo.magno@gmail.com",
+    "email_verified_at"=> "",
+    "password"=> "12345678",
+  ],
+];
