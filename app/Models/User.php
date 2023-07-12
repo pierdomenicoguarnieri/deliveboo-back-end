68bc8b38b0d2f43611cd06ad19c562886bd7a6e2
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-07-12 16:19:00 +0200

    add: models and relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,10 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function resturant(){
+      return $this->belongsTo(Resturant::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
