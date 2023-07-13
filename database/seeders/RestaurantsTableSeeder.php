1a41c3384a0acd71e5e7170caf03d7196e59d89a
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-07-13 15:06:55 +0200

    fix: relations and foreign keys, add seeders

--- database/seeders/RestaurantsTableSeeder.php
+++ database/seeders/RestaurantsTableSeeder.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Str;
 use App\Models\Restaurant;
 use App\Models\Dish;
 use Faker\Generator as Faker;
+use Mockery\Undefined;
 
 class RestaurantsTableSeeder extends Seeder
 {
@@ -20,30 +21,18 @@ class RestaurantsTableSeeder extends Seeder
     {
         $restaurants = config('restaurants');
 
-        $dishes = Dish::all();
-        $array_id = [];
-        foreach($dishes as $dish){
-            array_push($array_id, $dish['id']);
-        }
-
         foreach($restaurants as $restaurant){
-
-            $new_restaurant = new Restaurant();
-            
-            //$random_id = $faker->randomNumber(1, count($array_id));
-
-            //$new_restaurant->dish_id->attach($random_id);
-            //dd($new_restaurant);
-            $new_restaurant->name = $restaurant['name'];
-            $new_restaurant->slug = Str::slug($restaurant['name'], '-');
-            $new_restaurant->piva = $faker->numberBetween(10000000000, 99999999999);
-            $new_restaurant->email = $restaurant['email'];
-            $new_restaurant->telephone_number = $restaurant['telephone-number'];
-            $new_restaurant->address = $restaurant['address'];
-            $new_restaurant->image_path = $restaurant['image-path'];
-            $new_restaurant->image_name = $restaurant['image-name'];
-            $new_restaurant->rating = $faker->randomFloat(1, 1, 5);
-            $new_restaurant->save();
+          $new_restaurant = new Restaurant();
+          $new_restaurant->name = $restaurant['name'];
+          $new_restaurant->slug = Str::slug($restaurant['name'], '-');
+          $new_restaurant->piva = $faker->numberBetween(10000000000, 99999999999);
+          $new_restaurant->email = $restaurant['email'];
+          $new_restaurant->telephone_number = $restaurant['telephone-number'];
+          $new_restaurant->address = $restaurant['address'];
+          $new_restaurant->image_path = $restaurant['image-path'];
+          $new_restaurant->image_name = $restaurant['image-name'];
+          $new_restaurant->rating = $faker->randomFloat(1, 1, 5);
+          $new_restaurant->save();
         }
     }
 }
