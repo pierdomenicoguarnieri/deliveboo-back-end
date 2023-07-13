a6f8caab90994a9d6452e896957aab7b286d5cf0
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-07-13 16:38:20 +0200

    add: UsersTableSeeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    public function resturant(){
+    public function restaurant(){
       return $this->belongsTo(Restaurant::class);
     }
 
