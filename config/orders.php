ba603ae32a5c11d5d5c866975ff62d73925eb6fb
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-07-14 12:27:15 +0200

    add: orders.php in config and OrdersTableSeeder

---
+++ config/orders.php
@@ -0,0 +1,172 @@
+<?php
+
+  return [
+    [
+      "user_name" => "Giovanni",
+      "user_lastname" => "Giovannini",
+      "user_address" => "Via Garbatella, 54, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "giovanni.giovannini@gmail.com",
+      "tot_order" => 36.99
+    ],
+    [
+      "user_name" => "Mauro",
+      "user_lastname" => "Maurini",
+      "user_address" => "Via Roma, 33, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "mauro.maurini@gmail.com",
+      "tot_order" => 52.33
+    ],
+    [
+      "user_name" => "Laura",
+      "user_lastname" => "Laurini",
+      "user_address" => "Via Parioli, 22, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "laura.laurini@gmail.com",
+      "tot_order" => 12.50
+    ],
+    [
+      "user_name" => "Carlo",
+      "user_lastname" => "Carlini",
+      "user_address" => "Via Piramide, 95, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "carlo.carlini@gmail.com",
+      "tot_order" => 52.00
+    ],
+    [
+      "user_name" => "Giacomo",
+      "user_lastname" => "Giacomini",
+      "user_address" => "Via Trastevere, 68, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "giacomo.giacomini@gmail.com",
+      "tot_order" => 8.50
+    ],
+    [
+      "user_name" => "Lucia",
+      "user_lastname" => "Luciani",
+      "user_address" => "Via Testaccio, 11, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "lucia.luciani@gmail.com",
+      "tot_order" => 77.23
+    ],
+    [
+      "user_name" => "Rebecca",
+      "user_lastname" => "Rebecchini",
+      "user_address" => "Via Rebibbia, 78, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "rebecca.rebecchini@gmail.com",
+      "tot_order" => 37.62
+    ],
+    [
+      "user_name" => "Armando",
+      "user_lastname" => "Armandini",
+      "user_address" => "Via EUR, 26, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "armando.armandini@gmail.com",
+      "tot_order" => 66.50
+    ],
+    [
+      "user_name" => "Luca",
+      "user_lastname" => "Luchini",
+      "user_address" => "Via Ostia, 92, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "luca.luchini@gmail.com",
+      "tot_order" => 23.50
+    ],
+    [
+      "user_name" => "Sandro",
+      "user_lastname" => "Sandrini",
+      "user_address" => "Via Altare della Patria, 12, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "sandro.snadrini@gmail.com",
+      "tot_order" => 94.50
+    ],
+    [
+      "user_name" => "Alessandra",
+      "user_lastname" => "Alessandrini",
+      "user_address" => "Via Colosseo, 3, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "alessandra.alessandrini@gmail.com",
+      "tot_order" => 11.45
+    ],
+    [
+      "user_name" => "Ramona",
+      "user_lastname" => "Ramoni",
+      "user_address" => "Via Vaticano, 4, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "ramona.ramoni@gmail.com",
+      "tot_order" => 12.00
+    ],
+    [
+      "user_name" => "Ettore",
+      "user_lastname" => "Ettorini",
+      "user_address" => "Via Fontana di Trevi, 61, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "ettore.ettorini@gmail.com",
+      "tot_order" => 50.00
+    ],
+    [
+      "user_name" => "Achille",
+      "user_lastname" => "Achillini",
+      "user_address" => "Via Pantheon, 8, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "achille.achillini@gmail.com",
+      "tot_order" => 34.50
+    ],
+    [
+      "user_name" => "Asrdrubale",
+      "user_lastname" => "Asdrubalini",
+      "user_address" => "Via Monte Mario, 1, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "asdrubale.asdrubalini@gmail.com",
+      "tot_order" => 83.00
+    ],
+    [
+      "user_name" => "Stefano",
+      "user_lastname" => "Stefanini",
+      "user_address" => "Via Piazza Venezia, 33, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "stefano.stefanini@gmail.com",
+      "tot_order" => 88.00
+    ],
+    [
+      "user_name" => "Raffaele",
+      "user_lastname" => "Raffaelini",
+      "user_address" => "Via Condotti, 31, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "raffaele.raffaelini@gmail.com",
+      "tot_order" => 21.31
+    ],
+    [
+      "user_name" => "Marco",
+      "user_lastname" => "Marcolini",
+      "user_address" => "Via Basilica di San Paolo, 38, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "marco.marcolini@gmail.com",
+      "tot_order" => 42.33
+    ],
+    [
+      "user_name" => "Marica",
+      "user_lastname" => "Marichini",
+      "user_address" => "Via San Pasquale, 8, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "marica.marichini@gmail.com",
+      "tot_order" => 11.34
+    ],
+    [
+      "user_name" => "Andrea",
+      "user_lastname" => "Andreini",
+      "user_address" => "Via Marconi, 52, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "andrea.andreini@gmail.com",
+      "tot_order" => 11.00
+    ],
+    [
+      "user_name" => "Daniele",
+      "user_lastname" => "Danielini",
+      "user_address" => "Via Tiburtina, 8, Roma, RM",
+      "user_telephone_number" => "1234567890",
+      "user_email" => "daniele.danielini@gmail.com",
+      "tot_order" => 48.50
+    ],
+  ];
