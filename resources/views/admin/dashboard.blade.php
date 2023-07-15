5d6cdc571676f080ada0c2b73118a3bd081a1f66
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-07-15 18:40:25 +0200

    changed: minor graphic changes

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -21,21 +21,17 @@
       </h2>
       <div class="row justify-content-center">
           <div class="col">
-              <div class="card">
-                  <div class="w-100 p-2">Attenzione <strong>{{Auth::user()->name}}</strong>, non hai ancora aggiunto un ristorante!</div>
-
+              <div class="card boo-wrapper border-0">
                   <div class="card-body">
                       @if (session('status'))
                       <div class="alert alert-success" role="alert">
                           {{ session('status') }}
                       </div>
                       @endif
-                      <span class="py-3">Sei loggato con successo, però sembra che tu non abbia ancora aggiunto un ristorante!</span>
                       <div>
-                        <a href="{{route('admin.restaurants.create')}}" class="btn btn-primary">Aggiungi ora un ristorante!</a>
+                        <span class="d-block">Attenzione <strong>{{Auth::user()->name}}</strong>, non hai ancora aggiunto un ristorante!</span>
+                        <a href="{{route('admin.restaurants.create')}}" class="btn btn-success mt-3">Aggiungi ora un ristorante!</a>
                       </div>
-
-
                   </div>
               </div>
           </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,8 +4,8 @@
   <div class="container mt-4">
     <div class="row justify-content-center">
       <div class="col-md-8">
-        <div class="card">
-          <div class="card-header">{{ __('Register') }}</div>
+        <div class="card boo-wrapper">
+          <h4>{{ __('Register') }}</h4>
 
           <div class="card-body">
             <form method="POST" action="{{ route('register') }}">
