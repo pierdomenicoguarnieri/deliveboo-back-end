e0cf4261b16583ae69acd7168787f35f7439e45c
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-07-21 10:20:14 +0200

    add: link to front-end

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
   public function index(){
-    return view('guest.home');
+    return redirect('http://localhost:5174/');
   }
 }
