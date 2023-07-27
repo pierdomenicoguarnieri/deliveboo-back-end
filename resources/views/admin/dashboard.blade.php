99d4526e8926a6f4cf06cf54b8afa3b73847fbe7
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-07-27 16:15:35 +0200

    add last year graphic

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,13 +4,21 @@
 
   @section('content')
     <div class="container">
-      <div id="seven">
+      <div id="seven" class="graphic-hide">
         <canvas id="lastSeven"></canvas>
       </div>
       <div id="thirty">
         <canvas id="lastThirty" ></canvas>
       </div>
-      <button id="changeRange"></button>
+      <div id="year">
+        <canvas id="lastYear" ></canvas>
+      </div>
+      {{-- <button id="changeRange"></button> --}}
+      <select id="changeRange">
+        <option value="1">Sette giorni</option>
+        <option value="2">Trenta giorni</option>
+        <option value="3">Un anno</option>
+      </select>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -26,68 +34,90 @@
       const thirty              = document.getElementById('thirty');
                                   thirty.classList.add('graphic-hide');
       const lastThirty          = document.getElementById('lastThirty');
+      const year                = document.getElementById('year');
+                                  year.classList.add('graphic-hide');
+      const lastYear            = document.getElementById('lastYear');
       
-      const lastSevenDay        = [];
-      
+      const lastSevenDay        = [];     
       const lastSevenDate       = {
-        0: [],
-        1: [],
-        2: [],
-        3: [],
-        4: [],
-        5: [],
-        6: [],
-      };
+                                    0: [],
+                                    1: [],
+                                    2: [],
+                                    3: [],
+                                    4: [],
+                                    5: [],
+                                    6: [],
+                                  };
       const lastSevenDateCount  = [];
       const lastThirtyDay       = [];
       const lastThirtyDate      = {
-        0: [],
-        1: [],
-        2: [],
-        3: [],
-        4: [],
-        5: [],
-        6: [],
-        6: [],
-        7: [],
-        8: [],
-        9: [],
-        10: [],
-        11: [],
-        12: [],
-        13: [],
-        14: [],
-        15: [],
-        16: [],
-        17: [],
-        18: [],
-        19: [],
-        20: [],
-        21: [],
-        22: [],
-        23: [],
-        24: [],
-        25: [],
-        26: [],
-        27: [],
-        28: [],
-        29: [],
-      };
+                                    0: [],
+                                    1: [],
+                                    2: [],
+                                    3: [],
+                                    4: [],
+                                    5: [],
+                                    6: [],
+                                    6: [],
+                                    7: [],
+                                    8: [],
+                                    9: [],
+                                    10: [],
+                                    11: [],
+                                    12: [],
+                                    13: [],
+                                    14: [],
+                                    15: [],
+                                    16: [],
+                                    17: [],
+                                    18: [],
+                                    19: [],
+                                    20: [],
+                                    21: [],
+                                    22: [],
+                                    23: [],
+                                    24: [],
+                                    25: [],
+                                    26: [],
+                                    27: [],
+                                    28: [],
+                                    29: [],
+                                  };
       const lastThirtyDateCount = [];
+      const lastYearMonth       = [];
+      const lastYearDate        = {
+                                    0: [],
+                                    1: [],
+                                    2: [],
+                                    3: [],
+                                    4: [],
+                                    5: [],
+                                    6: [],
+                                    6: [],
+                                    7: [],
+                                    8: [],
+                                    9: [],
+                                    10: [],
+                                    11: [],
+                                  };
+      const lastYearDateCount   = [];
+      const ordersMonth         = [];
 
       const changeRange         = document.getElementById('changeRange');
-                                  changeRange.innerHTML = '30 giorni';
-                                  changeRange.addEventListener('click', function() {
-                                    if (thirty.classList.contains('graphic-hide')) {
-                                      seven.classList.add('graphic-hide');
-                                      thirty.classList.remove('graphic-hide');
-                                      changeRange.innerHTML = '7 giorni';
-                                    } else {
-                                      seven.classList.remove('graphic-hide');
-                                      thirty.classList.add('graphic-hide');
-                                      changeRange.innerHTML = '30 giorni';
-                                    }
-                                  });
+                                  // changeRange.innerHTML = '30 giorni';
+                                  // changeRange.addEventListener('click', function() {
+                                  //   if (thirty.classList.contains('graphic-hide')) {
+                                  //     seven.classList.add('graphic-hide');
+                                  //     thirty.classList.remove('graphic-hide');
+                                  //     changeRange.innerHTML = '7 giorni';
+                                  //   } else {
+                                  //     seven.classList.remove('graphic-hide');
+                                  //     thirty.classList.add('graphic-hide');
+                                  //     changeRange.innerHTML = '30 giorni';
+                                  //   }
+                                  // });
+
+
 
     
       // array ultimi sette giorni per le labels
@@ -132,6 +162,7 @@
 
 
 
+
       // array ultimi 30 giorni per le labels
       for (let i = 30; i >= 0; i--) {
         lastThirtyDay.push(dt.minus({days: i}).toISODate())
@@ -157,6 +188,56 @@
           datasets: [{
             label: 'Numero di ordini negli ultimi trenta giorni',
             data: lastThirtyDateCount,
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+              ticks: {
+                stepSize: 1
+              }
+            },
+          }
+        }
+      });
+
+
+
+
+      // array ultimo anno per le labels
+      for (let i = 11; i >= 0; i--) {
+        let month = dt.minus({months: i}).toISODate()
+        lastYearMonth.push(DateTime.fromISO(month).month)
+      }
+      
+      // lettura dei soli mesi degli ordini
+      orders.forEach(order => {
+        let orderDate = DateTime.fromISO(order);
+        let orderMonth = orderDate.month;
+        ordersMonth.push(orderMonth);
+      });
+      
+      // ordini dell'ultimo anno 
+      ordersMonth.forEach(order => {
+        for (let i = 11; i >= 0; i--) {
+          if (order == lastYearMonth[i]) lastYearDate[i].push(order);
+        }
+      })
+      
+      // count degli ordini dell'ultimo anno
+      for (const month in lastYearDate) {
+        lastYearDateCount.push(lastYearDate[month].length);
+      }
+
+      new Chart(lastYear, {
+        type: 'line',
+        data: {
+          labels: lastYearMonth,
+          datasets: [{
+            label: 'Numero di ordini dell\'ultimo anno',
+            data: lastYearDateCount,
             borderWidth: 1
           }]
         },
