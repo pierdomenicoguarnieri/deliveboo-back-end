f8882a5135808c0353b84cbfd08205c8cc7dcda5
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-07-27 11:29:49 +0200

    add: chart.js setup

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\DishOrder;
+use App\Models\Order;
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,6 +13,30 @@ class DashboardController extends Controller
 {
   public function index(){
     $restaurant = Restaurant::find(Auth::user()->restaurant_id);
-    return view('admin.dashboard', compact('restaurant'));
+    $ordersArray = [];
+    $orders = [];
+    $orders_ids = [];
+    $order_pivot = [];
+    $restaurant = (new Restaurant())->restaurantUser();
+    $dishes     = $restaurant->dishes()->get();
+    foreach($dishes as $dish){
+      $order_pivot    = DishOrder::where('dish_id', $dish->id)->get();
+      foreach ($order_pivot as $order) {
+        if(!in_array($order->order_id, $orders_ids)){
+          $orders_ids[] = $order->order_id;
+        }
+      }
+    }
+
+    asort($orders_ids);
+
+    foreach($orders_ids as $orderItem){
+      $order = Order::where('id', $orderItem)->with('dishes')->first();
+      if($order != null && !in_array($order, $orders)){
+        $orders[] = $order;
+      }
+    }
+
+    return view('admin.dashboard', compact('restaurant', 'orders'));
   }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,7 +3,50 @@
 @if (Auth::user()->restaurant_id != null && $restaurant != null)
 
   @section('content')
+    <div class="container">
+      <canvas id="myChart"></canvas>
+    </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/luxon/3.3.0/luxon.min.js'></script>
+
+    <script>
+      const DateTime = luxon.DateTime;
+      const orders = {!! json_encode($orders) !!};
+      const ctx = document.getElementById('myChart');
+
+      const dt = DateTime.now().set({ hour: 00, minutes: 00, seconds: 00, milliseconds: 00 }).setLocale('it');
+
+      console.log(dt.minus({days: 6}).toISODate());
+      console.log(dt.minus({days: 5}).toISODate());
+      console.log(dt.minus({days: 4}).toISODate());
+      console.log(dt.minus({days: 3}).toISODate());
+      console.log(dt.minus({days: 2}).toISODate());
+      console.log(dt.minus({days: 1}).toISODate());
+      console.log(dt.toISODate());
+
+      orders.forEach(order => {
+        console.log(order.created_at);
+      });
+      new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ['Lun', 'Mar', 'Mer', 'Giov', 'Ven', 'Sab', 'Dom'],
+          datasets: [{
+            label: 'Numero di ordini',
+            data: [12, 19, 3, 5, 2, 3, 12],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    </script>
   @endsection
 
   @elseif(Auth::user()->restaurant_id != null && $restaurant === null)
