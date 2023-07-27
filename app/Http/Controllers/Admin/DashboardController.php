dc9648a2d317e9ab0b940015b4b3a6b71a6e9d6e
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-07-27 15:11:54 +0200

    add graphics

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -12,13 +12,13 @@ use Illuminate\Support\Facades\Auth;
 class DashboardController extends Controller
 {
   public function index(){
-    $restaurant = Restaurant::find(Auth::user()->restaurant_id);
+    $restaurant  = Restaurant::find(Auth::user()->restaurant_id);
     $ordersArray = [];
-    $orders = [];
-    $orders_ids = [];
+    $orders      = [];
+    $orders_ids  = [];
     $order_pivot = [];
-    $restaurant = (new Restaurant())->restaurantUser();
-    $dishes     = $restaurant->dishes()->get();
+    $restaurant  = (new Restaurant())->restaurantUser();
+    $dishes      = $restaurant->dishes()->get();
     foreach($dishes as $dish){
       $order_pivot    = DishOrder::where('dish_id', $dish->id)->get();
       foreach ($order_pivot as $order) {
@@ -31,12 +31,12 @@ class DashboardController extends Controller
     asort($orders_ids);
 
     foreach($orders_ids as $orderItem){
-      $order = Order::where('id', $orderItem)->with('dishes')->first();
+      $order = Order::where('id', $orderItem)->pluck('created_at')->first();
       if($order != null && !in_array($order, $orders)){
-        $orders[] = $order;
+        $orders[] = $order->toDateString();
       }
     }
-
+    
     return view('admin.dashboard', compact('restaurant', 'orders'));
   }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,45 +4,170 @@
 
   @section('content')
     <div class="container">
-      <canvas id="myChart"></canvas>
+      <div id="seven">
+        <canvas id="lastSeven"></canvas>
+      </div>
+      <div id="thirty">
+        <canvas id="lastThirty" ></canvas>
+      </div>
+      <button id="changeRange"></button>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/luxon/3.3.0/luxon.min.js'></script>
 
     <script>
-      const DateTime = luxon.DateTime;
-      const orders = {!! json_encode($orders) !!};
-      const ctx = document.getElementById('myChart');
+      const orders              = {!! json_encode($orders) !!};
+      const DateTime            = luxon.DateTime;
+      const dt                  = DateTime.now().set({ hour: 00, minutes: 00, seconds: 00, milliseconds: 00 }).setLocale('it');
 
-      const dt = DateTime.now().set({ hour: 00, minutes: 00, seconds: 00, milliseconds: 00 }).setLocale('it');
+      const seven               = document.getElementById('seven');
+      const lastSeven           = document.getElementById('lastSeven');
+      const thirty              = document.getElementById('thirty');
+                                  thirty.classList.add('graphic-hide');
+      const lastThirty          = document.getElementById('lastThirty');
+      
+      const lastSevenDay        = [];
+      
+      const lastSevenDate       = {
+        0: [],
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: [],
+        6: [],
+      };
+      const lastSevenDateCount  = [];
+      const lastThirtyDay       = [];
+      const lastThirtyDate      = {
+        0: [],
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: [],
+        6: [],
+        6: [],
+        7: [],
+        8: [],
+        9: [],
+        10: [],
+        11: [],
+        12: [],
+        13: [],
+        14: [],
+        15: [],
+        16: [],
+        17: [],
+        18: [],
+        19: [],
+        20: [],
+        21: [],
+        22: [],
+        23: [],
+        24: [],
+        25: [],
+        26: [],
+        27: [],
+        28: [],
+        29: [],
+      };
+      const lastThirtyDateCount = [];
 
-      console.log(dt.minus({days: 6}).toISODate());
-      console.log(dt.minus({days: 5}).toISODate());
-      console.log(dt.minus({days: 4}).toISODate());
-      console.log(dt.minus({days: 3}).toISODate());
-      console.log(dt.minus({days: 2}).toISODate());
-      console.log(dt.minus({days: 1}).toISODate());
-      console.log(dt.toISODate());
+      const changeRange         = document.getElementById('changeRange');
+                                  changeRange.innerHTML = '30 giorni';
+                                  changeRange.addEventListener('click', function() {
+                                    if (thirty.classList.contains('graphic-hide')) {
+                                      seven.classList.add('graphic-hide');
+                                      thirty.classList.remove('graphic-hide');
+                                      changeRange.innerHTML = '7 giorni';
+                                    } else {
+                                      seven.classList.remove('graphic-hide');
+                                      thirty.classList.add('graphic-hide');
+                                      changeRange.innerHTML = '30 giorni';
+                                    }
+                                  });
 
+    
+      // array ultimi sette giorni per le labels
+      for (let i = 6; i >= 0; i--) {
+        lastSevenDay.push(dt.minus({days: i}).toISODate())
+      }
+      
+      //ordini degli ultimi sette giorni
       orders.forEach(order => {
-        console.log(order.created_at);
+        for (let i = 6; i >= 0; i--) {
+          if (order == dt.minus({days: i}).toISODate()) lastSevenDate[i].push(order);
+        }
       });
-      new Chart(ctx, {
+      
+      // count degli ordini degli ultimi sette giorni
+      for (const day in lastSevenDate) {
+        lastSevenDateCount.push(lastSevenDate[day].length);
+      }
+      lastSevenDateCount.reverse()
+
+      new Chart(lastSeven, {
         type: 'line',
         data: {
-          labels: ['Lun', 'Mar', 'Mer', 'Giov', 'Ven', 'Sab', 'Dom'],
+          labels: lastSevenDay,
           datasets: [{
-            label: 'Numero di ordini',
-            data: [12, 19, 3, 5, 2, 3, 12],
+            label: 'Numero di ordini negli ultimi sette giorni',
+            data: lastSevenDateCount,
             borderWidth: 1
           }]
         },
         options: {
           scales: {
             y: {
-              beginAtZero: true
-            }
+              beginAtZero: true,
+              ticks: {
+                stepSize: 1
+              }
+            },
+          }
+        }
+      });
+
+
+
+      // array ultimi 30 giorni per le labels
+      for (let i = 30; i >= 0; i--) {
+        lastThirtyDay.push(dt.minus({days: i}).toISODate())
+      }
+
+      // ordini degli ultimi 30 giorni
+      orders.forEach(order => {
+        for (let i = 30; i >= 0; i--) {
+          if (order == dt.minus({days: i}).toISODate()) lastThirtyDate[i].push(order);
+        }
+      });
+
+      // count degli ordini degli ultimi 30 giorni
+      for (const day in lastThirtyDate) {
+        lastThirtyDateCount.push(lastThirtyDate[day].length);
+      }
+      lastThirtyDateCount.reverse()
+
+      new Chart(lastThirty, {
+        type: 'line',
+        data: {
+          labels: lastThirtyDay,
+          datasets: [{
+            label: 'Numero di ordini negli ultimi trenta giorni',
+            data: lastThirtyDateCount,
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+              ticks: {
+                stepSize: 1
+              }
+            },
           }
         }
       });
